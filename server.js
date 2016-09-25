9893e44f5cd1e853dde1b7bbc75db5488cd91ffc
My1app <er.rajeshkumarsaini00@gmail.com>
2016-09-25 18:00:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-    res.send('The first URL information sent to you');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   
 });
 app.get('/article2',function(req,res){
     res.send("Article two information is sent to you");
