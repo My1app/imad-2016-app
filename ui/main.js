9734654885d0c15057c19ecf5efd12b45f0d6a4e
My1app <er.rajeshkumarsaini00@gmail.com>
2016-10-02 18:22:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+var button =document.getElementsById('counter');
+var counter=0;
+button.onclick=function(){
+  
+  
+  
+  counter = counter+1;
+  var span=document.getElementsById('count');
+  span.innerHTML=counter.toString();
+  
+};
